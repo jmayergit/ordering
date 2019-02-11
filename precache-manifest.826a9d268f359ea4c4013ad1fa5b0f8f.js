fd3fe7f587370af29d44e744eba1a7b0203b9562
Julien Mayer <jalexismayer@gmail.com>
2019-02-11 15:26:47 +0900

    Updates

---
+++ precache-manifest.826a9d268f359ea4c4013ad1fa5b0f8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f1920e41d824961a6aa",
+    "url": "/ordering/static/css/main.a54ec38f.chunk.css"
+  },
+  {
+    "revision": "4f1920e41d824961a6aa",
+    "url": "/ordering/static/js/main.4f1920e4.chunk.js"
+  },
+  {
+    "revision": "bdd2f1279f4b4e09eb97",
+    "url": "/ordering/static/js/1.bdd2f127.chunk.js"
+  },
+  {
+    "revision": "201270a0ab2c1216ccb0",
+    "url": "/ordering/static/js/runtime~main.201270a0.js"
+  },
+  {
+    "revision": "842df78b3ec4218d01fe3ddc911ef65c",
+    "url": "/ordering/index.html"
+  }
+];
